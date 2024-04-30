f4920e446751b232358b6fa5da5f3188845b1006
Ryo <91322302+cat394@users.noreply.github.com>
2024-04-30 10:29:10 +0900

    fix typo

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -2,7 +2,7 @@ import { RouteManager } from "../components";
 import { RouteLink } from "../components";
 declare global {
   interface HTMLElementTagNameMap {
-    "route-manager": RouteManage4;
+    "route-manager": RouteManager;
     "route-link": RouteLink;
   }
 }
