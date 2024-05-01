7d7485b3890a2d02d3452412ca9c74257328f6da
Ryo <91322302+cat394@users.noreply.github.com>
2024-05-01 14:46:35 +0900

    Fix duplicate custom element registration

--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,9 @@
-import "./components/index";
+import "./components";
 import { RouteManager } from "./components";
 import { RouteLink } from "./components";
 import { onAll, fallback } from "./constants";
 import { convertRouteConfig, path } from "./helpers";
 import type { RouteConfig, RouteHookContext, OnAllHookContext } from "./types";
-
-customElements.define("route-manager", RouteManager);
-customElements.define("route-link", RouteLink);
 
 export { RouteManager, RouteLink, onAll, fallback, convertRouteConfig, path };
 export type { RouteConfig, RouteHookContext, OnAllHookContext };
