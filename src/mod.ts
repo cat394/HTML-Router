26e5a368324fec1ad519c9867926a35fd82e3457
Ryo <91322302+cat394@users.noreply.github.com>
2024-05-02 16:38:45 +0900

    fix export Hook type

--- src/mod.ts
+++ src/mod.ts
@@ -3,7 +3,12 @@ import { RouteManager } from "./components";
 import { RouteLink } from "./components";
 import { onAll, fallback } from "./constants";
 import { convertRouteConfig, path } from "./helpers";
-import type { RouteConfig, RouteHookContext, OnAllHookContext } from "./types";
+import type {
+  RouteConfig,
+  RouteHookContext,
+  OnAllHookContext,
+  Hook,
+} from "./types";
 
 export { RouteManager, RouteLink, onAll, fallback, convertRouteConfig, path };
-export type { RouteConfig, RouteHookContext, OnAllHookContext };
+export type { RouteConfig, RouteHookContext, OnAllHookContext, Hook };
