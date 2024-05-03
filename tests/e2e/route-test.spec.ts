02d52d4c05841911dd8aa04c2eb659ebf07682f9
Ryo <91322302+cat394@users.noreply.github.com>
2024-05-03 19:08:52 +0900

    fix localhost number

--- tests/e2e/route-test.spec.ts
+++ tests/e2e/route-test.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-const localhost = "http://localhost:5174";
+const localhost = "http://localhost:5173";
 
 test("Home page test", async ({ page }) => {
   await page.goto(localhost);
